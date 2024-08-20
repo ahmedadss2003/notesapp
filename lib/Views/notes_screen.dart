86950d2bf7064b1d969ba3aff98b4c2fed394eec
ahmed <ashrafahmed13222@gmail.com>
2024-08-20 18:33:38 +0300

    change them and create notes view

---
+++ lib/Views/notes_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatelessWidget {
+  const NotesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title:const Text("Notes"),
+        actions: [
+          IconButton(onPressed: (){}, icon:const Icon(Icons.search))
+        ],
+      ),
+    );
+  }
+}
