70f88257caf8c7e7bd7fda72b74b99ec7a1d613a
ahmed <ashrafahmed13222@gmail.com>
2024-08-22 22:24:34 +0300

    validate user input

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -2,29 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/widgets/custom_button.dart';
 import 'package:notesapp/widgets/custom_text_form_field.dart';
 
-class AddNote extends StatelessWidget {
-  const AddNote({super.key});
+class AddNotebottomSheet extends StatelessWidget {
+  const AddNotebottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
+    return  Container(
+      padding:const EdgeInsets.all(10),
+      child:const AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  final GlobalKey<FormState> formKey =GlobalKey() ;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;// this is to check validate auto example=>use if user enter not validate input
+  String? title  , subtitle ;
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode, // this i need change if there was a problrm so i change to statfull 
       child: Column(
         
         children: [
         CustomTextField(
+          onSaved: (value){
+            title = value ;
+          },
           hint: "Tittle",
           horizontalPadding: 20,
           verticalPadding: 30,
         ),
          const SizedBox(height: 30,),
         CustomTextField(
+          onSaved: (value){
+            subtitle = value ;
+          },
           hint: "Content",
           horizontalPadding: 20,
           verticalPadding: 60,
         ),
         const SizedBox(height: 50,),
-        CustomButton(text: "Add")
+        CustomButton(
+          text: "Add",
+          onPressed: (){
+            if(formKey.currentState!.validate()){
+              formKey.currentState!.save();
+            }else{
+              setState(() {
+              autovalidateMode = AutovalidateMode.always ;      
+              });
+            }
+          },
+        )
         ],
       ),
     );
