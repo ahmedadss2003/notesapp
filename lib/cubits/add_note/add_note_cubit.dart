53cd4b52946d6970087da389fc317bf5006f17e4
ahmed <ashrafahmed13222@gmail.com>
2024-08-23 19:05:50 +0300

    add note method to add note cubit

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,8 +1,41 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notesapp/constants.dart';
+import 'package:notesapp/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+//  addNote(NoteModel note) async{
+//   emit(AddNoteLoading()) ;
+//   try {
+//   var notesBox = Hive.box(kNOteBox) ;
+//   emit(AddNoteSuccess());
+//   await notesBox.add(note);//add accept dynamic value so accept (note) but if note register notemodel not accept note ;
+// } catch (e) {
+//   emit(AddNoteFailure(errMessage: e.toString()));
+// }
+
+//  }
+
+  addNote(NoteModel note)async{
+  emit(AddNoteLoading()) ;
+  try{
+  var notesBox = Hive.box(kNOteBox);//This method is used to open an existing Hive box
+  await notesBox.add(note);
+  emit(AddNoteSuccess()) ;
+  }catch(e) {
+  emit(AddNoteFailure(errMessage: e.toString()));
+  }
+
 }
+}
+//Genetric method is very important if need use method with the diffrent type;
+/*
+ add<T>(T name){
+  return name ;
+ } 
+*/
